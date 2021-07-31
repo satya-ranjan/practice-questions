4444891c3082264040a2bd8a4df1e63f2b7e4753
satya-ranjan <satyasahoocs@gmail.com>
2021-07-31 20:48:41 +0530

    infosys

---
+++ Problem_27.py
@@ -0,0 +1,10 @@
+'''
+Write afunction to remove duplicat element from an array and return unique value only
+sample input :10,10,11,12,12,13,13,13
+sample output :10,11,12,13
+
+'''
+
+num = int(input("Enter the how many number you want to input : "))
+list1 = list(map(int, input("Enter input separeted by , ").strip().split(",")))[:num]
+print(set(list1))
